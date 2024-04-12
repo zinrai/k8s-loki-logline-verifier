13f1f903c5310c4a345fd458d9270a41ae4f943a
Hiroshi Umehara <zinrai0x19@gmail.com>
2024-04-12 14:13:06 +0900

    first import

---
+++ README.md
@@ -0,0 +1,110 @@
+# k8s-loki-logline-verifier
+
+This Go program is designed to verify the consistency between the number of log lines reported in Kubernetes pod annotations and the actual number of log lines retrieved from Loki.
+
+**This is a sample implementation of a program to check that the number of lines in the log output by k8s Pod matches the number of lines in the k8s Pod log stored in Loki.**
+
+## Motivation
+
+I wanted to check that the k8s Pod logs generated at [zinrai/k8s-pod-log-generator](https://github.com/zinrai/k8s-pod-log-generator) were stored in Loki without any missing logs.
+
+## Requirements
+
+- Annotations when [zinrai/k8s-pod-log-generator](https://github.com/zinrai/k8s-loki-logline-verifier) is executed Use the key and value of the number of rows in the attached log.
+- Access to a Grafana Loki instance with `auth_enabled: true`
+  - https://grafana.com/docs/loki/latest/configure/#supported-contents-and-default-values-of-lokiyaml
+- Access to Loki search endpoints deployed on k8s is required.
+- k8s Namespace is set as the unit of tenant id in Loki.
+
+Example of access to a Loki search endpoint using port forwarding:
+
+```
+$ kubectl get service -n loki
+NAME                        TYPE        CLUSTER-IP      EXTERNAL-IP   PORT(S)             AGE
+loki-backend                ClusterIP   10.24.124.202   <none>        3100/TCP,9095/TCP   20d
+loki-backend-headless       ClusterIP   None            <none>        3100/TCP,9095/TCP   20d
+loki-gateway                ClusterIP   10.24.120.118   <none>        80/TCP              20d
+loki-memberlist             ClusterIP   None            <none>        7946/TCP            20d
+loki-read                   ClusterIP   10.24.123.180   <none>        3100/TCP,9095/TCP   20d
+loki-read-headless          ClusterIP   None            <none>        3100/TCP,9095/TCP   20d
+loki-write                  ClusterIP   10.24.120.24    <none>        3100/TCP,9095/TCP   20d
+loki-write-headless         ClusterIP   None            <none>        3100/TCP,9095/TCP   20d
+query-scheduler-discovery   ClusterIP   None            <none>        3100/TCP,9095/TCP   20d
+$
+```
+```
+$ kubectl port-forward svc/loki-gateway 8080:80 -n loki
+Forwarding from 127.0.0.1:8080 -> 8080
+Forwarding from [::1]:8080 -> 8080
+Handling connection for 8080
+Handling connection for 8080
+```
+
+Example values.yaml for Promtail using Helm when logging from Cloud Pub/Sub:
+
+```
+daemonset:
+  enabled: false
+deployment:
+  enabled: true
+serviceAccount:
+  create: false
+  name: ksa-cloudpubsub
+configmap:
+  enabled: true
+config:
+  clients:
+    - url: http://loki-gateway.loki.svc.cluster.local/loki/api/v1/push
+      tenant_id: default
+  snippets:
+    scrapeConfigs: |
+      - job_name: gcplog
+        pipeline_stages:
+          - tenant:
+              label: "namespace"
+        gcplog:
+          subscription_type: "pull"
+          project_id: "project-id"
+          subscription: "subscription-id"
+          use_incoming_timestamp: false
+          use_full_line: false
+          labels:
+            job: "gcplog"
+        relabel_configs:
+          - source_labels: ['__gcp_resource_labels_namespace_name']
+            target_label: 'namespace'
+          - source_labels: ['__gcp_resource_labels_pod_name']
+            target_label: 'pod_name'
+```
+
+## Configuration
+
+The program reads configurations from a YAML file named `config.yaml`. The following configuration options are available:
+
+- `kubeconfig_path`: (Optional) Path to the Kubernetes cluster configuration file. If not provided, the default path will be used.
+- `namespace_prefix`: (Optional) Prefix for the namespaces created by the tool. Defaults to logger-ns.
+- `loki_address`: URL of the Loki server.
+
+## Usage
+
+```bash
+$ cat << EOF > config.yaml
+loki_address: "http://localhost:8080"
+EOF
+```
+
+```bash
+$ go run main.go
+2024/04/12 11:36:34 Match for pod logger-pod-13 in namespace logger-ns-1: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:34 Match for pod logger-pod-3 in namespace logger-ns-1: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:34 Match for pod logger-pod-1 in namespace logger-ns-2: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:34 Match for pod logger-pod-11 in namespace logger-ns-2: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:34 Match for pod logger-pod-12 in namespace logger-ns-3: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:35 Match for pod logger-pod-7 in namespace logger-ns-4: total_log_lines=2560, log_line_count=2560
+2024/04/12 11:36:35 Match for pod logger-pod-10 in namespace logger-ns-5: total_log_lines=2560, log_line_count=2560
+...
+```
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](https://opensource.org/license/mit) for details.
---
+++ main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"gopkg.in/yaml.v2"
+	"k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+type Config struct {
+	KubeconfigPath  string `yaml:"kubeconfig_path"`
+	NamespacePrefix string `yaml:"namespace_prefix"`
+	LokiAddress     string `yaml:"loki_address"`
+}
+
+type LokiQueryRangeRequest struct {
+	Query string `json:"query"`
+	Start int64  `json:"start"`
+	End   int64  `json:"end"`
+	Step  int    `json:"step"`
+}
+
+type LokiQueryRangeResponse struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric map[string]string `json:"metric"`
+			Values [][]interface{}   `json:"values"`
+		} `json:"result"`
+		Stats struct{} `json:"stats"`
+	} `json:"data"`
+}
+
+func getLogLineCount(podStartTime time.Time, podName string, namespace string, lokiAddress string) (int, error) {
+	startedAt := podStartTime.UnixNano()
+	end := podStartTime.Add(time.Hour).UnixNano() // 1 hour after pod start
+
+	query := fmt.Sprintf(`count_over_time({pod_name="%s"}[1h])`, podName)
+
+	u, err := url.Parse(fmt.Sprintf("%s/loki/api/v1/query_range", lokiAddress))
+	if err != nil {
+		return 0, err
+	}
+
+	params := url.Values{}
+	params.Set("query", query)
+	params.Set("start", strconv.FormatInt(startedAt, 10))
+	params.Set("end", strconv.FormatInt(end, 10))
+	params.Set("step", "1h")
+	u.RawQuery = params.Encode()
+
+	req, err := http.NewRequest("GET", u.String(), nil)
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("X-Scope-OrgID", namespace)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("Loki query failed with status code: %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	var lokiResp LokiQueryRangeResponse
+	err = json.Unmarshal(body, &lokiResp)
+	if err != nil {
+		return 0, err
+	}
+
+	if len(lokiResp.Data.Result) > 0 {
+		result, err := strconv.Atoi(lokiResp.Data.Result[0].Values[0][1].(string))
+		if err != nil {
+			return 0, err
+		}
+
+		return result, nil
+	}
+
+	return 0, fmt.Errorf("no logs found for pod %s", podName)
+}
+
+func main() {
+	configFile := "config.yaml"
+
+	configFileData, err := os.Open(configFile)
+	if err != nil {
+		log.Fatalf("Failed to open config file: %v", err)
+	}
+	defer configFileData.Close()
+
+	var config Config
+	decoder := yaml.NewDecoder(configFileData)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Failed to parse config file: %v", err)
+	}
+
+	if config.NamespacePrefix == "" {
+		config.NamespacePrefix = "logger-ns"
+	}
+
+	if config.KubeconfigPath == "" {
+		config.KubeconfigPath = filepath.Join(homedir.HomeDir(), ".kube", "config")
+	}
+
+	kubeconfig, err := clientcmd.BuildConfigFromFlags("", config.KubeconfigPath)
+	if err != nil {
+		log.Fatalf("Error building kubeconfig from %s: %v", config.KubeconfigPath, err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeconfig)
+	if err != nil {
+		log.Fatalf("Error creating Kubernetes client: %v", err)
+	}
+
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), v1.ListOptions{})
+	if err != nil {
+		log.Fatalf("Failed to list namespaces: %v", err)
+	}
+
+	for _, namespace := range namespaces.Items {
+		if !isTargetNamespace(namespace.Name, config.NamespacePrefix) {
+			continue
+		}
+
+		pods, err := clientset.CoreV1().Pods(namespace.Name).List(context.TODO(), v1.ListOptions{})
+		if err != nil {
+			log.Fatalf("Failed to list pods in namespace %s: %v", namespace.Name, err)
+		}
+
+		for _, pod := range pods.Items {
+			totalLogLines, err := strconv.Atoi(pod.Annotations["total_log_lines"])
+			if err != nil {
+				log.Printf("Failed to get total log lines for pod %s in namespace %s: %v", pod.Name, namespace.Name, err)
+				continue
+			}
+
+			logLineCount, err := getLogLineCount(pod.Status.StartTime.Time, pod.Name, namespace.Name, config.LokiAddress)
+			if err != nil {
+				log.Printf("Failed to get log line count for pod %s in namespace %s: %v", pod.Name, namespace.Name, err)
+				continue
+			}
+
+			if totalLogLines != logLineCount {
+				log.Printf("Mismatch for pod %s in namespace %s: total_log_lines=%d, log_line_count=%d", pod.Name, namespace.Name, totalLogLines, logLineCount)
+			} else {
+				log.Printf("Match for pod %s in namespace %s: total_log_lines=%d, log_line_count=%d", pod.Name, namespace.Name, totalLogLines, logLineCount)
+			}
+		}
+	}
+}
+
+func isTargetNamespace(namespaceName, namespacePrefix string) bool {
+	return len(namespaceName) >= len(namespacePrefix) && namespaceName[:len(namespacePrefix)] == namespacePrefix
+}
